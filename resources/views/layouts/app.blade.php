3ec3c8951db3f848636c5925cbef537abb1b100a
Muhammad Usman <usmanshoaib362@gmail.com>
2026-04-07 16:45:03 +0500

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="base-url" content="{{ url('/') }}">
+    <script>window.BASE_URL = '{{ url('/') }}';</script>
     @php $appFavicon = \App\Models\Setting::get('favicon'); @endphp
     @if($appFavicon)
     <link rel="icon" href="{{ $appFavicon }}" type="image/png">
